6bf2f7d49e7c9e06e6a5aae87e0e713535726056
Manabu TERADA <terada@cmscom.jp>
2026-02-03 12:57:21 +0900

    01 - 19 までを見直して再実行。CLIPのモデルのデータ保存に問題があった模様

--- src/image_vector_poc/evaluation/__init__.py
+++ src/image_vector_poc/evaluation/__init__.py
@@ -2,10 +2,13 @@
 
 from .metrics import EvaluationMetrics, compute_distance_ratio, evaluate_embeddings
 from .reporter import EvaluationReporter
+from .similarity_search import SimilaritySearchMetrics, evaluate_similarity_search
 
 __all__ = [
     "EvaluationMetrics",
     "EvaluationReporter",
+    "SimilaritySearchMetrics",
     "compute_distance_ratio",
     "evaluate_embeddings",
+    "evaluate_similarity_search",
 ]
